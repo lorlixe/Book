20a01fd4ac953198246f65adc6b2d9bdaa91a8b9
Marine Kouadio <marinekouadio@gmail.com>
2023-11-23 23:26:01 +0100

    Hébergement de la BDD

--- back/models/index.js
+++ back/models/index.js
@@ -5,21 +5,16 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_HOST,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    { dialect: "mysql" }
-  );
-}
+
+sequelize = new Sequelize(
+  process.env.DB_HOST,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  { dialect: "mysql" }
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
